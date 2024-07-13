79fb461007454cd5143aa47e05c2a510c36d7411
Dheeren29 <85298596+Dheeren29@users.noreply.github.com>
2024-07-13 20:42:31 +0530

    Update Area program.py

--- Area program.py
+++ Area program.py
@@ -1,4 +1,6 @@
 def area_tri(base,height):
      area=(base*height)/2
-     print("the area is: "+ str(area))
+     #print("the area is: "+ str(area))
+     print("I love my india")
 area_tri(6,3)
+area_tri(10, 2)

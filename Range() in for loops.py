5abc31fccc0416dde452cd8b16ffbf2d5e8ca4c1
Dheeren29 <85298596+Dheeren29@users.noreply.github.com>
2024-07-13 20:05:49 +0530

    Update Range() in for loops.py

--- Range() in for loops.py
+++ Range() in for loops.py
@@ -1,3 +1,9 @@
+def area_tri(base,height):
+     area=(base*height)/2
+     print("the area is: "+ str(area))
+area_tri(6,3)
+//This above code is only added to practice merging a branch on Git Hub. The above code is for the Area program.
+
 '''for i in range(10):
  print(i)'''
 
